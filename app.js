aa9048e3d80f9883dc0475d2bd372e6458f39116
Patel <j_patel73637@tss-hr420-dm37.fc.ca>
2019-03-07 17:51:11 -0500

    Demo

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\nThis is NodeJs'); // res.end i slike a php echo statement
+  res.setHeader('Content-Type', 'text/html');
+
+  let url = req.url;
+  //set up some routes
+  if(url== "/about"){
+    res.write(`<h1>Hello NodeJs ${url} page </h1>`);
+    res.end();
+  }else if(url == "/contact"){
+    res.write(`<h1>Hello NodeJs ${url} page </h1>`);
+    res.end();
+  }else{
+    res.write(`<h1>Hello NodeJs ${port}`);
+    res.end(); // res.end i slike a php echo statement
+  }
+
 });
 
 server.listen(port, hostname, () => {
---
+++ test/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', message: 'This is first NodeJs Express Page'});
+});
+
+module.exports = router;
